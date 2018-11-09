38fd4b622f7bd43c8451a0b5272f0ac55854294c
Felipe <dudmont1@gmail.com>
2018-11-09 04:22:45 -0200

    Add existing file

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -6,22 +6,59 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Digite o primeiro algarismo");
+            string Algarismo1 = Console.ReadLine();
+            decimal Algarismo1emdecimal = Convert.ToDecimal(Algarismo1);
+
+            Console.WriteLine("Digite o operador");
             string operador = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo algarimo");
+            string Algarismo2 = Console.ReadLine();
+            decimal Algarismo2emdecimal = Convert.ToDecimal(Algarismo2);
+
+            decimal resultado = 0;
+
+            decimal resto = 0;
 
             if(operador== "+")
             {
+                resultado =
+                    Algarismo1emdecimal +
+                    Algarismo2emdecimal;
             }
             else if (operador == "-")
             {
+                resultado =
+                    Algarismo1emdecimal -
+                    Algarismo2emdecimal;
             }
             else if (operador == "*")
             {
+                resultado =
+                    Algarismo1emdecimal *
+                    Algarismo2emdecimal;
             }
             else if (operador == "/")
             {
+                resultado =
+                    Algarismo1emdecimal /
+                    Algarismo2emdecimal;
+
+                resto =
+                    Algarismo1emdecimal %
+                    Algarismo2emdecimal;
+                Console.WriteLine("O resto da divisão é =" + resto);
             }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("O resultado foi = " + resultado);
+            if (resultado % 2 == 0)
+                Console.WriteLine("par");
+            else
+                Console.WriteLine("impar");
+            Console.ReadKey();
+           
         }
     }
 }
